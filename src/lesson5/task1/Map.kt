42ce05062097a7ba034951d8a2761bf2bd2f11c9
Вадим <vadim-gryaznykh@yandex.ru>
2018-10-28 21:31:55 +0300

    что то из 5 урока

--- src/lesson5/task1/Map.kt
+++ src/lesson5/task1/Map.kt
@@ -94,7 +94,13 @@ fun buildWordSet(text: List<String>): MutableSet<String> {
  *     mapOf("Emergency" to "911", "Police" to "02")
  *   ) -> mapOf("Emergency" to "112, 911", "Police" to "02")
  */
-fun mergePhoneBooks(mapA: Map<String, String>, mapB: Map<String, String>): Map<String, String> = TODO()
+fun mergePhoneBooks(mapA: Map<String, String>, mapB: Map<String, String>): Map<String, String> {
+    var name = mapA.toMutableMap()
+    for ((i, x) in mapB)
+        if ((name[i] != x) && (name[i] != null)) name[i] = "${name[i]}, $x"
+        else name[i] = x
+    return name
+}
 
 /**
  * Простая
@@ -106,7 +112,16 @@ fun mergePhoneBooks(mapA: Map<String, String>, mapB: Map<String, String>): Map<S
  *   buildGrades(mapOf("Марат" to 3, "Семён" to 5, "Михаил" to 5))
  *     -> mapOf(5 to listOf("Семён", "Михаил"), 3 to listOf("Марат"))
  */
-fun buildGrades(grades: Map<String, Int>): Map<Int, List<String>> = TODO()
+fun buildGrades(grades: Map<String, Int>): Map<Int, List<String>> {
+    var x = mutableMapOf<Int, List<String>>()
+    grades.forEach {
+        if (x[it.value] == null) x[it.value] = listOf(it.key)
+        else x[it.value] = x[it.value]!! + it.key
+    }
+    for ((i, k) in x)
+        x[i] = k.sortedDescending()
+    return x
+}
 
 /**
  * Простая
@@ -118,7 +133,10 @@ fun buildGrades(grades: Map<String, Int>): Map<Int, List<String>> = TODO()
  *   containsIn(mapOf("a" to "z"), mapOf("a" to "z", "b" to "sweet")) -> true
  *   containsIn(mapOf("a" to "z"), mapOf("a" to "zee", "b" to "sweet")) -> false
  */
-fun containsIn(a: Map<String, String>, b: Map<String, String>): Boolean = TODO()
+fun containsIn(a: Map<String, String>, b: Map<String, String>): Boolean {
+    for ((i, x) in a) if (b[i] != x) return false
+    return true
+}
 
 /**
  * Средняя
@@ -189,14 +207,23 @@ fun propagateHandshakes(friends: Map<String, Set<String>>): Map<String, Set<Stri
  *   subtractOf(a = mutableMapOf("a" to "z"), mapOf("a" to "z"))
  *     -> a changes to mutableMapOf() aka becomes empty
  */
-fun subtractOf(a: MutableMap<String, String>, b: Map<String, String>): Unit = TODO()
+fun subtractOf(a: MutableMap<String, String>, b: Map<String, String>): Unit {
+    for ((i, x) in b) {
+        if (a[i] == x) a.remove(i)
+    }
+}
 
 /**
  * Простая
  *
  * Для двух списков людей найти людей, встречающихся в обоих списках
  */
-fun whoAreInBoth(a: List<String>, b: List<String>): List<String> = TODO()
+fun whoAreInBoth(a: List<String>, b: List<String>): List<String> {
+    var list = mutableSetOf<String>()
+    list.addAll(a)
+    list.retainAll(b)
+    return list.toList()
+}
 
 /**
  * Средняя
